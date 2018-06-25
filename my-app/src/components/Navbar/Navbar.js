1fc428698990d8a1d5c18668e2848d4a832332cf
Yumna Akhtar <yumnaakhtar@hotmail.com>
2018-06-25 15:08:12 -0700

    change pictures

--- my-app/src/components/Navbar/Navbar.js
+++ my-app/src/components/Navbar/Navbar.js
@@ -5,20 +5,19 @@ import "./Navbar.css";
 
 const Navbar = props => (
   // <div className="navbar">{props.children}>
-<div className="sticky-nav">
-<div className="sticky-jumbo">
-  <div className="jumbotron jumbotron-fluid">
-  <div className="container nav-title">
-    <h1> Memory Game</h1>
-    <p className="lead">Play this game ans test your memory. All you have to do is make sure you don't click the same character twice.. Good Luck!</p>
-    <a className="navbar-brand nav-restart" href="/">Restart</a>
-    <div className="status" >Current Score: {props.current}</div>
-    <div className="status" >High Score: {props.high}</div>
-    
+  <div className="sticky-nav">
+    <div className="sticky-jumbo">
+      <div className="jumbotron jumbotron-fluid">
+        <div className="container nav-title">
+          <h1> Memory Game</h1>
+          <p className="lead">Play this game ans test your memory. All you have to do is make sure you don't click the same character twice.. Good Luck!</p>
+          <a className="navbar-brand nav-restart" href="/">Restart</a>
+          <div className="status" >High Score: {props.high}</div>
+          <div className="status" >Current Score: {props.current}</div>
+        </div>
+      </div>
+    </div>
   </div>
-</div>
-</div>
-</div>
 );
 
 export default Navbar;
